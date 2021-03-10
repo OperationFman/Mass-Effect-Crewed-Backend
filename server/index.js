084d215e84a166a3c16dbe8cea6ca04202dc188d
Franklin Moon <franklin.m.moon@gmail.com>
2021-03-10 15:35:27 +1100

    node reading save data with temp file

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require('fs');
 
+let rawdata = fs.readFileSync('./server/saves/franklin.m.moon.json');
+let userData = JSON.parse(rawdata);
+console.log(userData);
+
 app.get('/api', (req, res) => {
     res.json({message: "Hello from server!" });
 });
