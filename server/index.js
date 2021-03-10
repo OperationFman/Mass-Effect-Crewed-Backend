61a0f09456bf9ecc937fa8de74b242a5ecaa5bf8
Franklin Moon <franklin.m.moon@gmail.com>
2021-03-10 15:22:33 +1100

    Importing filesystem

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-
 const PORT = process.env.PORT || 3001;
 const app = express();
+const fs = require('fs');
 
 app.get('/api', (req, res) => {
     res.json({message: "Hello from server!" });
